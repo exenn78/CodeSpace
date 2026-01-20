3c6ed194e4c6d07fcfdd24287b7b090b2dcc73ed
Ewenn Carcaillon <ewenn.kark78@icloud.com>
2026-01-20 13:10:20 +0000

    maj

---
+++ cours/commands.sql
@@ -0,0 +1,12 @@
+--CREATE TABLE Joueur(nom VARCHAR(255), PRENOM VARCHAR(255),
+--couleurMaillot CHAR(10),salaire NUMERIC(7, 2), tel VARCHAR(20)
+--);
+
+-- copier le fichier dans /tmp/...
+-- exécuter avec \i /tmp/.... dans 
+
+
+--CREATE TABLE Eleve ( nom VARCHAR (255) PRIMARY KEY , prenom VARCHAR (255)
+--UNIQUE NOT NULL , naissance DATE , age INT , CHECK ( age >= 18) ) ;
+
+--SELECT espece, nom FROM animal;
